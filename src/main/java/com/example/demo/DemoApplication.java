8e52a0fb4694906ee5fe3a57c00414e21c71bd43
jouureee <jouureee@gmail.com>
2022-01-12 17:49:03 +0900

    Add StudentController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,21 +11,12 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(1L, "spqjf12345",
-						"spqjf12345@gmail.com",
-						LocalDate.of(2000, Month.JANUARY, 5),
-						21)
-		);
-	}
+
 
 }
