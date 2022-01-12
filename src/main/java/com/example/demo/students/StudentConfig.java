82e453bba62ef4475f4cfd0615be6bf156a855ad
jouureee <jouureee@gmail.com>
2022-01-12 21:27:07 +0900

    Add Student database postgres

---
+++ src/main/java/com/example/demo/students/StudentConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.students;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student spq = new Student(
+                    1L, "spqjf12345", "spqjf12345@gmail.com",
+                    LocalDate.of(1999, Month.JANUARY, 18), 21
+            );
+            Student cindy = new Student(
+                    1L, "cindy", "cindy@gmail.com",
+                    LocalDate.of(1998, Month.FEBRUARY, 19), 22
+            );
+
+            repository.saveAll(
+                    List.of(spq, cindy)
+            );
+        };
+
+
+    };
+}
--- src/main/java/com/example/demo/students/StudentService.java
+++ src/main/java/com/example/demo/students/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.students;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,13 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L, "spqjf12345",
-                        "spqjf12345@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21)
-        );
+        return studentRepository.findAll();
     }
 }
